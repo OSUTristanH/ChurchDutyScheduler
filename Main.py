3418bca7f6dd4641c0c914ca1901fadd1a5be99d
OSUTristanH <harvillt@oregonstate.edu>
2021-02-05 00:48:12 -0600

    Initial commit

---
+++ Main.py
@@ -0,0 +1,6 @@
+# Author: Tristan Harville
+# Date: 02-05-2021
+# Description: Creates schedules for church duties through the next month. Can edit member preferences, member absences,
+# and add/delete members.
+
+
